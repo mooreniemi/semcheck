bde493cae0f3eed7894c317a76527100266bb846
Alex Moore-Niemi <amooreniemi@zipcar.com>
2015-10-21 17:05:46 -0400

    wip schema searching

--- lib/semcheck.rb
+++ lib/semcheck.rb
@@ -7,7 +7,7 @@ module Semcheck
   class Application
     THESAURUS_API_KEY =  "jbvSLnAGjO55Im0KsKU4"
     THESAURUS_API_HOST = "http://thesaurus.altervista.org/thesaurus/v1"
-    attr_accessor :terms
+    attr_accessor :terms, :synonyms, :schemas
 
     def initialize(args)
       @terms = args
@@ -15,31 +15,45 @@ module Semcheck
 
     def run
       puts "Searching semweb resources for: #{terms}"
-      synonyms = RestClient.get(
-        THESAURUS_API_HOST,
-        {
-          :params => {
-            'word' => terms,
-            'language' => 'en_US',
-            'key' => THESAURUS_API_KEY
-          },
-          :content_type => :json
-        }
-      ) do |response, request, result, &block|
-        case result.code
-        when 404
-          []
-        else
-          maybe_list = Crack::XML.parse(
-            response
-          )["response"]["list"]
-          (
-            maybe_list.size == 2 ? maybe_list["synonyms"] : maybe_list.first["synonyms"]
-          ).to_s.split("|")
+      @synonyms = []
+      terms.split(",").each do |term|
+        RestClient.get(
+          THESAURUS_API_HOST,
+          {
+            :params => {
+              'word' => term,
+              'language' => 'en_US',
+              'key' => THESAURUS_API_KEY
+            },
+            :content_type => :json
+          }
+        ) do |response, request, result, &block|
+          case result.code
+          when 404
+            []
+          else
+            maybe_list = Crack::XML.parse(
+              response
+            )["response"]["list"]
+            @synonyms << (
+              maybe_list.size == 2 ? maybe_list["synonyms"] : maybe_list.first["synonyms"]
+            ).to_s.split("|")
+          end
         end
       end
 
-      return synonyms
+      schema_org_search_form = agent.get("https://schema.org/").form("gsc-search-box")
+      results = agent.submit(schema_org_search_form, schema_org_search_form.buttons.first)
+puts results
+      return self
+    end
+
+    private
+    def all_candidates
+      terms + synonyms
+    end
+    def agent
+      @agent = Mechanize.new
     end
   end
 end
--- spec/semcheck_spec.rb
+++ spec/semcheck_spec.rb
@@ -11,13 +11,30 @@ describe Semcheck do
       expect(Semcheck::Application.new("house").terms).to eq("house")
     end
     describe "#run" do
+      let(:restaurant_synonyms) do
+        ["eating house","eating place", "building", "edifice"]
+      end
+      let(:house_synonyms) do
+        ["dwelling", "home", "domicile", "abode", "habitation", "dwelling house", "building", "edifice"]
+      end
+
       it 'returns terms to stdout' do
         expect { Semcheck::Application.new("house").run }.
           to output("Searching semweb resources for: house\n").to_stdout
       end
+      it 'gets synonyms for a term' do
+        # compare to how much better the results are from
+        # http://www.thesaurus.com/browse/restaurant?s=t
+        expect(Semcheck::Application.new("restaurant").run.synonyms).
+          to eq([restaurant_synonyms])
+      end
       it 'gets synonyms for terms' do
-        expect(Semcheck::Application.new("restaurant").run).
-          to eq(["eating house","eating place", "building", "edifice"])
+        expect(Semcheck::Application.new("restaurant,house").run.synonyms).
+          to eq([restaurant_synonyms, house_synonyms])
+      end
+      it 'gets potential schema matches for terms and synonyms' do
+        expect(Semcheck::Application.new("house").run.schemas).
+          to eq(["HousePainter", "Residence", "RentAction", "JobPosting", "Organization"])
       end
     end
   end
