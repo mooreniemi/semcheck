e978cf6b76cd8b4d40a9ba4ab37e57698dfdf358
Alex Moore-Niemi <amooreniemi@zipcar.com>
2015-10-21 15:07:50 -0400

    basic spec and app

--- lib/semcheck.rb
+++ lib/semcheck.rb
@@ -1,8 +1,15 @@
-require "semcheck/version"
+require_relative "semcheck/version"
 
 module Semcheck
   class Application
+    attr_accessor :terms
+
     def initialize(args)
+      @terms = args
+    end
+
+    def run
+      puts "you gave me #{terms}"
     end
   end
 end
--- spec/semcheck_spec.rb
+++ spec/semcheck_spec.rb
@@ -5,7 +5,15 @@ describe Semcheck do
     expect(Semcheck::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "Application" do
+    it 'can be initialized with command line args' do
+      expect(Semcheck::Application.new("foo")).to be_a(Semcheck::Application)
+      expect(Semcheck::Application.new("foo").terms).to eq("foo")
+    end
+    describe "#run" do
+      it 'returns terms' do
+        expect{ Semcheck::Application.new("foo").run }.to output("you gave me foo\n").to_stdout
+      end
+    end
   end
 end
